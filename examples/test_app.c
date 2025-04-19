d4edc63baaaebf04c41fac8a0f72bb41bb1ebbb3
lnxknl <jjmutong@gmail.com>
2025-04-20 00:37:15 +0800

    update

--- examples/test_app.c
+++ examples/test_app.c
@@ -1,6 +1,15 @@
 #include "lvgl.h"
-#include "lv_drivers/sdl/sdl.h"
+#include "SDL2/SDL.h"
 #include <unistd.h>
+#include <stdlib.h>
+
+static SDL_Window * window;
+static SDL_Renderer * renderer;
+static SDL_Texture * texture;
+static uint32_t * pixels;
+
+#define DISP_HOR_RES 800
+#define DISP_VER_RES 480
 
 static void btn_event_cb(lv_event_t * e)
 {
@@ -10,16 +19,118 @@ static void btn_event_cb(lv_event_t * e)
     }
 }
 
+static void flush_cb(lv_display_t * disp, const lv_area_t * area, uint8_t * px_map)
+{
+    int32_t x, y;
+    uint32_t w = (area->x2 - area->x1 + 1);
+    uint32_t h = (area->y2 - area->y1 + 1);
+
+    // Copy the rendered pixels to the texture
+    for(y = 0; y < h; y++) {
+        for(x = 0; x < w; x++) {
+            int idx = y * w + x;
+            uint32_t color = *((uint32_t*)&px_map[idx * 4]);
+            pixels[(area->y1 + y) * DISP_HOR_RES + area->x1 + x] = color;
+        }
+    }
+
+    SDL_UpdateTexture(texture, NULL, pixels, DISP_HOR_RES * 4);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+
+    lv_display_flush_ready(disp);
+}
+
 static void hal_init(void)
 {
     // Initialize SDL
-    sdl_init();
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return;
+    }
 
-    // SDL driver will handle display and input device initialization
-    lv_display_t * disp = lv_sdl_get_display();
-    lv_indev_t * mouse = lv_sdl_get_mouse();
-    lv_indev_t * mousewheel = lv_sdl_get_mousewheel();
-    lv_indev_t * keyboard = lv_sdl_get_keyboard();
+    window = SDL_CreateWindow("LVGL Test",
+                            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                            DISP_HOR_RES, DISP_VER_RES,
+                            SDL_WINDOW_SHOWN);
+    if(!window) {
+        printf("Window creation error: %s\n", SDL_GetError());
+        return;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(!renderer) {
+        printf("Renderer creation error: %s\n", SDL_GetError());
+        return;
+    }
+
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
+                              SDL_TEXTUREACCESS_STREAMING, DISP_HOR_RES, DISP_VER_RES);
+    if(!texture) {
+        printf("Texture creation error: %s\n", SDL_GetError());
+        return;
+    }
+
+    pixels = (uint32_t*)malloc(DISP_HOR_RES * DISP_VER_RES * 4);
+    if(!pixels) {
+        printf("Memory allocation error!\n");
+        return;
+    }
+
+    // Initialize LVGL display
+    static uint32_t buf[DISP_HOR_RES * DISP_VER_RES];  // Use 32-bit buffer
+    lv_display_t * disp = lv_display_create(DISP_HOR_RES, DISP_VER_RES);
+    lv_display_set_buffers(disp, buf, NULL, DISP_HOR_RES * DISP_VER_RES * 4, LV_DISPLAY_RENDER_MODE_PARTIAL);
+    lv_display_set_flush_cb(disp, flush_cb);
+
+    // Set the display color format
+    lv_display_set_color_format(disp, LV_COLOR_FORMAT_ARGB8888);
+}
+
+static void handle_mouse_events(void)
+{
+    static lv_indev_t * mouse_indev = NULL;
+    static lv_point_t last_point = {0, 0};
+    static bool last_pressed = false;
+
+    if (mouse_indev == NULL) {
+        mouse_indev = lv_indev_create();
+        lv_indev_set_type(mouse_indev, LV_INDEV_TYPE_POINTER);
+    }
+
+    SDL_Event event;
+    bool pressed = last_pressed;
+    lv_point_t point = last_point;
+
+    while(SDL_PollEvent(&event)) {
+        switch(event.type) {
+            case SDL_MOUSEBUTTONDOWN:
+                pressed = true;
+                point.x = event.button.x;
+                point.y = event.button.y;
+                break;
+            case SDL_MOUSEBUTTONUP:
+                pressed = false;
+                point.x = event.button.x;
+                point.y = event.button.y;
+                break;
+            case SDL_MOUSEMOTION:
+                point.x = event.motion.x;
+                point.y = event.motion.y;
+                pressed = (event.motion.state & SDL_BUTTON_LMASK) != 0;
+                break;
+            case SDL_QUIT:
+                SDL_Quit();
+                exit(0);
+                break;
+        }
+    }
+
+    last_point = point;
+    last_pressed = pressed;
+
+    lv_indev_set_data(mouse_indev, &point, pressed ? LV_INDEV_STATE_PRESSED : LV_INDEV_STATE_RELEASED);
 }
 
 int main(void)
@@ -47,9 +158,9 @@ int main(void)
     
     // Main loop
     while(1) {
+        handle_mouse_events();
         lv_timer_handler();
-        lv_sdl_window_handle_events();
-        usleep(5000);
+        SDL_Delay(5);
     }
     
     return 0;
